2d1bfcc0f8a84f4568290a2e11a23c910de057a2
Rivan Albani Ray <rivanalbaniray88@gmail.com>
2023-04-03 10:57:44 +0700

    done media and dll

---
+++ MyMediaPlayer/app/src/main/java/com/dicoding/mymediaplayer/MediaPlayerCallback.kt
@@ -0,0 +1,6 @@
+package com.dicoding.mymediaplayer
+
+interface MediaPlayerCallback {
+    fun onPlay()
+    fun onStop()
+}
---
+++ TicketingApps/app/src/main/java/com/dicoding/ticketingapps/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.dicoding.ticketingapps
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+import android.widget.Button
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+        val seatView = findViewById<SeatsView>(R.id.seatsView)
+        val button = findViewById<Button>(R.id.finishButton)
+
+        button.setOnClickListener {
+            seatView.seat?.let {
+                Toast.makeText(this, "Kursi Anda nomor ${it.name}.", Toast.LENGTH_SHORT).show()
+            } ?: run {
+                Toast.makeText(this, "Silakan pilih kursi terlebih dahulu.", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+    }
+}
